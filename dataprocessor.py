c7d778a50847726248656b2a3121cd11ebdf8bfc
Jason Liu <jason@Jasons-M2-Book.local>
2024-04-07 16:18:29 -0700

    final

---
+++ dataprocessor.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import os
+
+def processData(folderpath):
+    fileList = os.listdir(folderpath)
+    df = pd.DataFrame(columns=['type', 'lyrics'])
+    for filepath in fileList:
+        try:
+            with open(folderpath + '/' + filepath, 'r') as f:
+                lyricstring = f.read().strip()
+                df.loc[len(df.index)] = [lyricstring[0], lyricstring[2:]]
+        except:
+            pass
+
+def classifyString(s):
+    s = s.split(' ')
+    d = {}
+    for word in s:
+        if word not in d and s != 'dinosaur':
+            d[word] = 1
+        else:
+            d[word] += 1    
+
+    return d
+
+def classifyCSV(t, csvfile):
+    df = pd.read_csv(csvfile)
+    totalDict = {}
+
+    for i in range(len(list(df.get('lyrics')))):
+        if df.get('type')[i] == t:
+            oneDict = classifyString(df.get('lyrics')[i])
+            for key in oneDict:
+                if key not in totalDict:
+                    totalDict[key] = oneDict[key]
+                else:
+                    totalDict[key] += oneDict[key]
+    
+    return totalDict
+
+def testClassify(s):
+    data = 'training_data.csv'
+    class0 = classifyCSV(0, data)
+    class1 = classifyCSV(1, data)
+    class2 = classifyCSV(2, data)
+
+    type0 = 0
+    type1 = 0
+    type2 = 0
+
+    s = s.split(' ')
+    for word in s:
+        if word in class0.keys():
+            type0 += class0[word]
+        if word in class1:
+            type1 += class1[word]
+    if type0 > type1 and type0 > type2:
+        return "Old person"
+    elif type1 > type2:
+        return "Dinosaur!"
+    else:
+        return "Other use"
--- stringcleaner.py
+++ stringcleaner.py
@@ -1,29 +1,48 @@
 import nltk
 import nltk.corpus
-from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-nltk.download('punkt')
 
-stopwordlist = stopwords.words('english')
+stopwordlist = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 
+'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
+'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 
+'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+ 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are',
+ 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
+ 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 
+'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 
+'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 
+'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 
+'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 
+'s', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
+ 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 
+'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn',
+ "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 
+'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+ps = PorterStemmer()
 
 def formatString(input):
     # Split and remove all stop words
-    output = input.split(' ')
-    output = [word for word in output if word not in stopwordlist]
+    input = input.split(' ')
+    input = [word for word in input if word not in stopwordlist]
 
     # Change words to be singular and breaks down words in different tenses
-    for word in output:
-        word = PorterStemmer().stem(word)
-        word = WordNetLemmatizer.lemmatize(word)
+    output = []
+    for word in input:
+        output.append(ps.stem(word))
     
     output = ' '.join(output)
     return output
 
+# Formats a given file with the lyric string
 def formatFile(filepath):
     inputString = ''
     with open(filepath, 'r') as f:
-        inputString = filepath.read()
+        inputString = f.read()
     
     with open(filepath, 'w') as f:
         f.write(formatString(inputString))
+
+# Sources:
+# https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
+# https://www.geeksforgeeks.org/python-stemming-words-with-nltk/ 
